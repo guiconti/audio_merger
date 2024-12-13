104814fb8fa07a9a26446b83ddf0b2f0b5cb132c
root <guibasconti@gmail.com>
2024-12-13 02:53:32 +0000

    Decode base 64 before sending

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def test():
 			final_audio.export(audio_buffer)
 			audio_buffer = audio_buffer.getbuffer()
 	merged_audio_base_64 = base64.b64encode(audio_buffer)
-	return jsonify({"merged_audio_base_64": merged_audio_base_64})
+	return jsonify({"merged_audio_base_64": merged_audio_base_64.decode('ascii')})
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
